1caddd800b89c6c53de53385ddd5472e58ce1d66
LboroDigihub <166396910+LboroDigihub@users.noreply.github.com>
2024-04-09 10:31:28 +0100

    ToolbarContainer.js

--- src/react-components/ToolbarContainer.js
+++ src/react-components/ToolbarContainer.js
@@ -28,14 +28,14 @@ export function ToolbarContainer({ onGLBUploaded, randomizeConfig }) {
             </>
           }
         ></MoreMenu>
-        <button onClick={randomizeConfig}>Randomize avatar</button>
+        <button onClick={randomizeConfig}>Randomise avatar</button>
         <button onClick={dispatchResetView}>Reset camera view</button>
         <button onClick={dispatchExportAvatar} className="primary">
           Export avatar
         </button>
       </div>
       <div className="toolbarNotice">
-        <span>The 3D models used in this app are ©2020-2022 by individual mozilla.org contributors and modified by the DigiHub Research Group at Loughborough University.
+        <span>The models used in this app are ©2020-2022 by individual mozilla.org contributors and modified by the DigiHub Research Group at Loughborough University.
           Content available under a <a href="https://www.mozilla.org/en-US/foundation/licensing/website-content/" target="_blank" noreferrer>Creative Commons license</a>.</span>
       </div>
     </Toolbar>
